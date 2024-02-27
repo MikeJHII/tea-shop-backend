5fa655d8eb7cc29b250e473b51a844b7b7b52d2f
Michael Jordan-Hotchkiss II <michaeljordan-hotchkissii@Jordans-Mac-mini.local>
2024-02-27 03:43:50 -0600

    updated settings.py

--- config/settings.py
+++ config/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['*']
 # CORS_ALLOW_ALL_ORIGINS = True 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
+    'https://mike-teashop.netlify.app/',
 ]
 
 # CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:5173']
